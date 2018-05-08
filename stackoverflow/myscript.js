ce332a61636a22b068fbcea555c7b441e27eef61
Harshini N <radhaharshini@gmail.com>
2018-05-08 10:49:33 +0530

    Add button to google.com

---
+++ stackoverflow/myscript.js
@@ -0,0 +1,8 @@
+if (document.title.indexOf("Google") != -1) {
+    //Creating Elements
+    var btn = document.createElement("BUTTON")
+    var t = document.createTextNode("CLICK ME");
+    btn.appendChild(t);
+    //Appending to DOM 
+    document.body.appendChild(btn);
+}
