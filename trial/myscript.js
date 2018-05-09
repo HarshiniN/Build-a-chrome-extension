0fce43cc8828e492241845d60ff7d8c94744ac58
Harshini N <radhaharshini@gmail.com>
2018-05-09 11:09:22 +0530

    Update

--- trial/myscript.js
+++ trial/myscript.js
@@ -7,12 +7,6 @@ button.icon = 'favicon.png';
 	button.style.width = '315px';
 	button.style.backgroundColor = 'DodgerBlue';
 	button.style.color = 'white';
-	//button.appendChild(document.createTextNode ("\n"));
-	//button.appendChild (document.createElement ("span"));
-	//document.body.appendChild(button);
-	//var d = document.getElementsByClassName("thing-download-btn");
-	//var d = document.getElementsByClassName("thing-page-right");
-	//d.insertBefore(button, d.firstChild);
 	var gap = document.createElement("div");
 	gap.width = '40px';
 	gap.height = '40px';
@@ -21,14 +15,3 @@ button.icon = 'favicon.png';
 	gap.style.color = 'whitesmoke';
 	document.getElementsByClassName("thing-page-right")[0].prepend(gap);
 	document.getElementsByClassName("thing-page-right")[0].prepend(button);
-/*
-    var buttonOnSite = document.getElementById("buttonOnSite"),
-    parent = buttonOnSite.parentElement,
-    next = buttonOnSite.nextSibling,
-    button = document.createElement("button"),
-    text = document.createTextNode("test");
-
-button.appendChild(text);
-if (next) parent.insertBefore(button, next);
-else parent.appendChild(button);
-*/
